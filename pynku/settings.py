b2d9069c9720fb39f0953543d055ad194b9df5f8
tiominga <tiominga@gmail.com>
2025-05-08 20:01:41 -0300

    first deploy

--- pynku/settings.py
+++ pynku/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1n3=$-)(4+m&c0@3fqva70)krg5ux=@m#1n0*^15gok-8)a+qv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
